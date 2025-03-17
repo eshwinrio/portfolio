9d92e3b680f7f91d700209c28462b264c721a5ec
Eshwin Rio <riopinto487@gmail.com>
2025-03-17 13:51:52 -0400

    Add new SVG icons: Conestoga, RazorDigitalSolutions, and THEMUSEUM

--- ui/profile-overview.tsx
+++ ui/profile-overview.tsx
@@ -13,24 +13,28 @@ import SocialButtonsStack from "./social-buttons-stack";
 export default function ProfileOverview() {
     return (
         <Grid2 container spacing={2}>
-            <Grid2 md={6}>
-                <Box sx={{ mb: 2 }}>
-                    <Avatar sx={{ width: 164, height: 164 }}>
-                        <Image
-                            src="/20211225_172819-PFP.jpg"
-                            alt="Portrait of Eshwin Rio Pinto"
-                            width={164} height={164}
-                            style={{ width: "100%", height: "100%" }}
-                            aria-label="Portrait of Eshwin Rio Pinto"
-                        />
-                    </Avatar>
-                </Box>
-                <Typography component="h1" variant="h3" gutterBottom lineHeight={0.9}>
-                    <Box component="small" fontWeight="bold" display="block">Eshwin Rio</Box>
-                    <Box component="small" color="text.secondary">Pinto</Box>
-                </Typography>
+            <Grid2 container alignItems="center" xs={12} md={6}>
+                <Grid2>
+                    <Box sx={{ mb: 2 }}>
+                        <Avatar sx={{ aspectRatio: "1 / 1", width: { xs: 96, md: 124 }, height: "auto" }}>
+                            <Image
+                                src="/20211225_172819-PFP.jpg"
+                                alt="Portrait of Eshwin Rio Pinto"
+                                width={164} height={164}
+                                style={{ width: "100%", height: "100%" }}
+                                aria-label="Portrait of Eshwin Rio Pinto"
+                            />
+                        </Avatar>
+                    </Box>
+                </Grid2>
+                <Grid2>
+                    <Typography component="h1" variant="h3" gutterBottom lineHeight={0.9}>
+                        <Box component="small" fontWeight="bold" display="block">Eshwin Rio</Box>
+                        <Box component="small" color="text.secondary">Pinto</Box>
+                    </Typography>
+                </Grid2>
             </Grid2>
-            <Grid2 md={6}>
+            <Grid2 xs={12} md={6}>
                 <Typography variant="h5" gutterBottom>
                     <Box component="b">Full-Stack Developer</Box><br />
                     <Box component="span" color="text.secondary">based in <Box component="b">Kitchener-Waterloo</Box>🍁</Box>
