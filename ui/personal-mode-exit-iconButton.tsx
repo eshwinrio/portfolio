419b4980b45b530f19e6083f75c27c02074a8173
Eshwin Rio <riopinto487@gmail.com>
2025-03-19 11:53:37 -0400

    Add personal mode exit button and personal home page content

---
+++ ui/personal-mode-exit-iconButton.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
+import Fade from "@mui/material/Fade";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FC } from "react";
+
+
+const PersonalModeExitIconButton: FC = () => {
+    const isPersonalModeActive = usePathname().startsWith("/personal");
+
+    return (
+        <Fade in={isPersonalModeActive} unmountOnExit>
+            <Tooltip
+                arrow
+                title={
+                    <Stack alignItems="center">
+                        <Image
+                            src="/fb9aa87393ea093949ae640f0644f34c_9115808074601848149.png"
+                            width={64}
+                            height={64}
+                            alt=""
+                            aria-hidden="true"
+                        />
+                        <Typography variant="body2">Exit personal mode?</Typography>
+                    </Stack>
+                }
+            >
+                <IconButton
+                    component={Link}
+                    href="/"
+                    aria-label="Exit personal mode and return to professional portfolio"
+                >
+                    <SwapHorizIcon />
+                </IconButton>
+            </Tooltip>
+        </Fade>
+    );
+};
+
+export default PersonalModeExitIconButton;
