782ff9c2f9468a0747800a073e61aee0d9b57320
Eshwin Rio <riopinto487@gmail.com>
2025-03-25 19:43:40 -0400

    Refactor header layout to use Grid2 for improved responsiveness and update project card tag spacing

--- ui/header.tsx
+++ ui/header.tsx
@@ -1,6 +1,6 @@
 import AppBar, { AppBarProps } from "@mui/material/AppBar";
-import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
+import Grid2 from "@mui/material/Unstable_Grid2";
 import { FC } from "react";
 import Brand from "./brand";
 import MyTZTimeViewer from "./mytz-time-viewer";
@@ -11,16 +11,20 @@ import PersonalModeExitIconButton from "./personal-mode-exit-iconButton";
 const Header: FC<AppBarProps> = () => {
     return (
         <AppBar position="fixed">
-            <Container>
-                <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
-                    <Brand sx={{ display: { xs: "none", sm: "block" } }} />
-                    <MyTZTimeViewer />
-                    <div>
+            <Toolbar>
+                <Grid2 container alignItems="center" sx={{ width: "100%" }}>
+                    <Grid2 sm={4} sx={{ display: { xs: "none", sm: "block" } }}>
+                        <Brand />
+                    </Grid2>
+                    <Grid2 xs={true} sm={4} sx={{ display: "flex", justifyContent: { xs: "flex-start", sm: "center" } }}>
+                        <MyTZTimeViewer />
+                    </Grid2>
+                    <Grid2 xs="auto" sm={4} sx={{ display: "flex", justifyContent: "flex-end" }}>
                         <PersonalModeExitIconButton />
-                        <PaletteModeToggleIconButton />
-                    </div>
-                </Toolbar>
-            </Container>
+                        <PaletteModeToggleIconButton edge="end" />
+                    </Grid2>
+                </Grid2>
+            </Toolbar>
         </AppBar>
     );
 }
